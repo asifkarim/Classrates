fa0b359530219e2221a5c1969958b029ceaa73fe
Karim <asif.karim@willistowerswatson.com>
2018-06-07 09:49:04 +0100

    First commit

--- public/scripts/functions.js
+++ public/scripts/functions.js
@@ -1,7 +1,7 @@
 function submitComment(commentControl) {
     var comments = document.getElementsByName(commentControl)[0].value;
-    var subscriptionKey = "TEXT_ANALYTICS_API_KEY";
-    var url = "TEXT_ANALYTICS_ENDPOINT/sentiment";
+    var subscriptionKey = "25c4c2487543445f9917301f85aecfcf";
+    var url = "https://westcentralus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment";
 
     var payload = '{ "documents": [ { "language": "en-US", "id": "1", "text": "' + comments + '" }]}';
 
